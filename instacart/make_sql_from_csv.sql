3af1d04bdcd2b7b18838ed866d91234a5ba3e478
Stephen Privitera <privitera@so1.net>
2017-08-14 14:05:33 +0200

    SQL script to convert csv to sqlite
    pandas is really horrible with memory usage and has been crashing
    my computer repeatedly. I hope sqlite will work better ...
    
    cat make_sql_from_csv.sql | sqlite3 instacart.sqlite

---
+++ instacart/make_sql_from_csv.sql
@@ -0,0 +1,7 @@
+.mode csv
+.import aisles.csv aisles
+.import order_products__prior.csv order_products__prior
+.import order_products__train.csv order_products__train
+.import orders.csv orders
+.import departments.csv departments
+.import products.csv products
